923f23950d1152ac6e189402bd886205e66c9bc9
ttronexe2 <adria.baltrons@gracia.lasalle.cat>
2024-11-18 16:01:58 +0100

    feat: :sparkles: Test 3: Learn More

---
+++ tests/learnmore.test.js
@@ -0,0 +1,16 @@
+const { test, expect } = require('@playwright/test');
+
+// Comprueba el Lea
+test('Learn More', async ({ page }) => {
+  await page.goto('https://www.werfen.com/es/es');
+  
+  //Accptar las cookies
+  const acceptCookiesButton = await page.locator('button:has-text("Aceptar")');
+  if (await acceptCookiesButton.isVisible()) {
+  await acceptCookiesButton.click();
+  }
+  
+  await page.getByRole('link', { name: 'APRENDE MÁS' }).click();
+  await expect(page).toHaveURL('https://www.werfen.com/es/es/our-purpose-vision-and-values');
+  await expect(page).toHaveTitle(/Our Purpose | Werfen en España/);
+});
